80a034bd9e32ff98f1b368d196ce26f9512403e1
Iman Anooshehpour <imananoosheh@gmail.com>
2022-10-06 16:48:33 -0700

    Added and impld. state manag. for all fields to persist data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { AccountForm } from "./AccountForm";
 import { AddressForm } from "./AddressForm";
 import "./App.css";
 import { useMultiStepForm } from "./useMultiStepForm";
 import { Userform } from "./UserForm";
 
+type FormData = {
+  firstName: string;
+  lastName: string;
+  age: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  email: string;
+  password: string;
+};
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+};
+
 function App() {
+  const [data, setData] = useState(INITIAL_DATA);
+  function updateFields(fields: Partial<FormData>) {
+    setData(previousData => {
+      return { ...previousData, ...fields}
+    })
+  }
   const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
-    useMultiStepForm([<Userform />, <AddressForm />, <AccountForm />]);
+    useMultiStepForm([
+      <Userform {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />,
+    ]);
 
   function onSubmit(event: FormEvent) {
     event.preventDefault();
